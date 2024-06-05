5a26f0762ac3c199a10b26494616a135e32098d3
DukeBas <duke.bas@gmail.com>
2024-06-05 13:07:38 +0200

    Preliminary setup benchmarking

---
+++ bench.bat
@@ -0,0 +1,3 @@
+cabal build
+.\dist-newstyle\build\x86_64-windows\ghc-9.4.8\HaskellLatencyHiding-0.1.0.0\x\HaskellLatencyHiding\build\HaskellLatencyHiding\HaskellLatencyHiding.exe --output result.html -L 300
+@REM This is a bit of a hack, but it allows for quicker iteration.
--- src/main.hs
+++ src/main.hs
@@ -6,12 +6,32 @@ import Control.Concurrent.Async
 import Delay
 import Hiding
 
-main :: IO ()
-main = do
-  result <- delayedTreeReduce 0 delayedPlus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  putStrLn "Result: "
-  print result
+-- Benchmarking
+import Criterion.Main
 
--- Example operator, the + with a uniform random delay between 0 and 100 microseconds
+main :: IO ()
+main =
+  defaultMain
+    [ 
+      bgroup "test" [
+        bench "delayedTreeReduce" $ whnfIO (delayedTreeReduce 0 delayedPlus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]),
+        bench "delayedReduce" $ whnfIO (delayedReduce 0 delayedPlus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]),
+        bench "delayedFoldLReduce" $ whnfIO (delayedFoldLReduce 0 delayedPlus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]),
+        bench "delayedFoldRReduce" $ whnfIO (delayedFoldRReduce 0 delayedPlus [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+      ]
+    ]
+
+-- Helper function to create a benchmark group for a given reducer (always with DelayedPlus)
+-- tests the 4 variants of the reducer with on the same input list
+createBenchGroupPlus :: String -> [Int] -> [Benchmark]
+createBenchGroupPlus name input_list = [
+  bench (name ++ "-delayedTreeReduce") $ whnfIO (delayedTreeReduce 0 delayedPlus input_list),
+  bench (name ++ "-delayedReduce") $ whnfIO (delayedReduce 0 delayedPlus input_list),
+  bench (name ++ "-delayedFoldLReduce") $ whnfIO (delayedFoldLReduce 0 delayedPlus input_list),
+  bench (name ++ "-delayedFoldRReduce") $ whnfIO (delayedFoldRReduce 0 delayedPlus input_list)
+  ]
+
+
+-- Example operator, the + with a uniform random delay between 0.1 and 0.2 seconds
 delayedPlus :: Int -> Int -> IO (Async Int)
-delayedPlus = wrapDelay (uniformDelay 0.9 1) (+)
+delayedPlus = wrapDelay (uniformDelay 0.1 0.2) (+)
